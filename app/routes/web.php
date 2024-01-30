0462d86c1fce7125a91eec14eae5bd756d62c4bd
MuhammadSyed2 <123975019+MuhammadSyed2@users.noreply.github.com>
2024-01-30 18:47:59 +0600

    npm install and build issue

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GuitarsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,22 @@ use App\Http\Controllers\HomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/', [HomeController::class, 'index'])->name('homeIndex');
 Route::get('/about', [HomeController::class, 'about'])->name('homeAbout');
